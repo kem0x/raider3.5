0e83cbffbdf1568fe789168a9f3aa9f11d9f629d
kemo <kareemolim@gmail.com>
2022-05-22 18:24:44 +0200

    put emoting back

--- Raider/ufunctionhooks.h
+++ Raider/ufunctionhooks.h
@@ -202,19 +202,67 @@ namespace UFunctionHooks
         })
 
         DEFINE_PEHOOK("Function FortniteGame.FortPlayerController.ServerPlayEmoteItem", {
+            if (!Object->IsA(AFortPlayerControllerAthena::StaticClass()))
+                return;
+
             auto CurrentPC = (AFortPlayerControllerAthena*)Object;
             auto CurrentPawn = (APlayerPawn_Athena_C*)CurrentPC->Pawn;
 
             auto EmoteParams = (AFortPlayerController_ServerPlayEmoteItem_Params*)Parameters;
+            auto AnimInstance = (UFortAnimInstance*)CurrentPawn->Mesh->GetAnimInstance();
 
-            if (CurrentPC && !CurrentPC->IsInAircraft() && CurrentPawn && EmoteParams->EmoteAsset)
+            if (CurrentPC && !CurrentPC->IsInAircraft() && CurrentPawn && EmoteParams->EmoteAsset && !AnimInstance->bIsJumping && !AnimInstance->bIsFalling)
             {
-                if (auto Montage = EmoteParams->EmoteAsset->GetAnimationHardReference(CurrentPawn->CharacterBodyType, CurrentPawn->CharacterGender))
+                if (EmoteParams->EmoteAsset->IsA(UAthenaDanceItemDefinition::StaticClass()))
                 {
-                    CurrentPawn->PlayLocalAnimMontage(Montage, 1.0f, FName(0));
-                    CurrentPawn->PlayAnimMontage(Montage, 1.0f, FName(0));
-                    CurrentPawn->OnRep_CharPartAnimMontageInfo();
-                    CurrentPawn->OnRep_ReplicatedAnimMontage();
+                    if (auto Montage = EmoteParams->EmoteAsset->GetAnimationHardReference(CurrentPawn->CharacterBodyType, CurrentPawn->CharacterGender))
+                    {
+                        if (AnimInstance && Montage)
+                        {
+                            auto& RepAnimMontageInfo = CurrentPawn->RepAnimMontageInfo;
+                            auto& RepCharPartAnimMontageInfo = CurrentPawn->RepCharPartAnimMontageInfo;
+                            auto& LocalAnimMontageInfo = CurrentPawn->AbilitySystemComponent->LocalAnimMontageInfo;
+                            auto Ability = CurrentPawn->AbilitySystemComponent->AllReplicatedInstancedAbilities[0];
+
+                            const auto Duration = AnimInstance->Montage_Play(Montage, 1.0f, EMontagePlayReturnType::Duration, 0.0f, true);
+
+                            if (Duration > 0.f)
+                            {
+                                LocalAnimMontageInfo.AnimMontage = Montage;
+                                if(Ability)
+                                {
+                                    LocalAnimMontageInfo.AnimatingAbility = Ability;
+                                }
+                                LocalAnimMontageInfo.PlayBit = 1;
+
+                                RepAnimMontageInfo.AnimMontage = Montage;
+                                RepAnimMontageInfo.ForcePlayBit = 1;
+
+                                RepCharPartAnimMontageInfo.PawnMontage = Montage;
+
+                                if(Ability)
+                                {
+                                    Ability->CurrentMontage = Montage;
+                                }
+
+                                bool bIsStopped = AnimInstance->Montage_GetIsStopped(Montage);
+
+                                if (!bIsStopped)
+                                {
+                                    RepAnimMontageInfo.PlayRate = AnimInstance->Montage_GetPlayRate(Montage);
+                                    RepAnimMontageInfo.Position = AnimInstance->Montage_GetPosition(Montage);
+                                    RepAnimMontageInfo.BlendTime = AnimInstance->Montage_GetBlendTime(Montage);
+                                }
+
+                                RepAnimMontageInfo.IsStopped = bIsStopped;
+                                RepAnimMontageInfo.NextSectionID = 0;
+
+                                CurrentPawn->OnRep_ReplicatedMovement();
+                                CurrentPawn->OnRep_CharPartAnimMontageInfo();
+                                CurrentPawn->OnRep_ReplicatedAnimMontage();
+                            }
+                        }
+                    }
                 }
             }
         })
