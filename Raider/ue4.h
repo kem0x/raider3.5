80ebff4946569837708f8eed178605476966dbf5
kemo <kareemolim@gmail.com>
2022-05-31 15:06:39 +0200

    brain damage :skull:

--- Raider/ue4.h
+++ Raider/ue4.h
@@ -658,7 +658,7 @@ static void GrantGameplayAbility(APlayerPawn_Athena_C* TargetPawn, UClass* Gamep
     {
         auto& CurrentSpec = AbilitySystemComponent->ActivatableAbilities.Items[i];
 
-        if (CurrentSpec.Ability->GetFullName() == Spec.Ability->GetFullName()) // Player already has this ability // TODO: not do getfullname
+        if (CurrentSpec.Ability == Spec.Ability)
 			return;
     }
 
